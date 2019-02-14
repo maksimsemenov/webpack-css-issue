e307e6e4c42a89eb7942c9e8b73bd7ddee8e090d
Maksim Semenov <maksim.semenov@elationhealth.com>
2019-02-14 07:20:24 -0500

    Update config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,44 +1,45 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'index.js',
-    library: 'Vital',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, './dist-dev'),
+    filename: "index.js",
+    library: "Vital",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, "./dist-dev")
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
         // Needed for blueprint. See https://stackoverflow.com/a/45192304
         test: /\.(eot|ttf|woff|woff2)$/,
-        loader: `${require.resolve('file-loader')}?name=fonts/[name].[ext]`,
-      },
-    ],
+        loader: `${require.resolve("file-loader")}?name=fonts/[name].[ext]`
+      }
+    ]
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         styles: {
+          name: "styles",
           test: /\.css$/,
-          chunks: 'all',
-          enforce: true,
-        },
-      },
-    },
+          chunks: "all",
+          enforce: true
+        }
+      }
+    }
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'vital-blueprint.css',
+      filename: "vital-blueprint.css"
     }),
-    new CleanWebpackPlugin(['./dist-dev']),
-  ],
+    new CleanWebpackPlugin(["./dist-dev"])
+  ]
 };
